fa92e467d2e3180b6bac0ad67228d248b59f6502
Jhon Ever Diaz <jediaz66@ucatolica.edu.co>
2024-02-10 17:10:27 -0500

    Init repository

---
+++ src/main/java/com/mycompany/aves/Ave.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aves;
+
+/**
+ *
+ * @author jhon
+ */
+public abstract class Ave {
+    public abstract void makeSound();
+}
---
+++ src/main/java/com/mycompany/aves/Aves.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aves;
+
+/**
+ *
+ * @author jhon
+ */
+public class Aves {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
